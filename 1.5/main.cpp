3f2d36850a8bed81f27694358870fc0987e364ad
Kevin <56335026+kevinumarov@users.noreply.github.com>
2021-05-27 13:36:48 +0900

    Add files via upload

---
+++ 1.5/main.cpp
@@ -0,0 +1,19 @@
+// Created by Kevin, March 22,2021
+// Student number: 201739103
+// Introduction to Programming with C++
+/// Exercise 1.5,
+/// Chapter 1.
+
+/* (Compute Expressions) Write a program that displays the result of ((1.2 * 0.1) + (3.3 * 0.3)) / (0.09 + 0.001)
+ */
+
+// Pseudo code
+// I. Including the header files
+// II. Printing the result by using the cout statement
+// III. Display the output
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"The result of ((1.2 * 0.1) + (3.3 * 0.3)) / (0.09 + 0.001) is:"<< ((1.2 * 0.1) + (3.3 * 0.3)) / (0.09 + 0.001)<< endl;
+    return 0;
+}
