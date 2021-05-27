fcc03e5ccfd1b2133c46bb8f61eae15c06f3067b
Kevin <56335026+kevinumarov@users.noreply.github.com>
2021-05-27 13:34:52 +0900

    Add files via upload

---
+++ 1.3/main.cpp
@@ -0,0 +1,19 @@
+// Created by Kevin, March 22,2021
+// Student number: 201739103
+// Introduction to Programming with C++
+/// Exercise 1.3,
+/// Chapter 1.
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<  " * "  <<endl;
+    cout<<  "***" <<endl;
+    cout<< "*****" <<endl;
+    cout<<"*******" <<endl;
+    cout<<"*******" <<endl;
+    cout<< "*****" <<endl;
+    cout<<  "***" <<endl;
+    cout<<  " * "  <<endl;
+    return 0;
+}
