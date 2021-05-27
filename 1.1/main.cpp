7bbffad42b29c84a83bf7f9cd66a2a4eb29c2d54
Kevin <56335026+kevinumarov@users.noreply.github.com>
2021-05-27 13:34:07 +0900

    Add files via upload

---
+++ 1.1/main.cpp
@@ -0,0 +1,13 @@
+// Created by Kevin, March 22,2021
+// Student number: 201739103
+// Introduction to Programming with C++
+/// Exercise 1.1, Chapter 1.
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout <<"Introduction to computer engineering"<<endl;
+    cout <<"Programming with C++ is fun, but not funnier than Swift" <<endl;
+    cout << "Actually, Swift is better"<<endl;
+    return 0;
+}
