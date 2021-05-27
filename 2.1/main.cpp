700261c3a04b0e6385e07538e7f5e5c94dfb2491
Kevin <56335026+kevinumarov@users.noreply.github.com>
2021-05-27 13:56:17 +0900

    Add files via upload

---
+++ 2.1/main.cpp
@@ -0,0 +1,12 @@
+// Created by Kevin
+// Student number is 201739103
+// MArch 22,2021
+// C++ programming
+// Chapter 2
+// Exercise 2.1
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
