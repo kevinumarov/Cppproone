ee7fa2020f0d79f27c86b4240cb8cfbd465a41b8
Kevin <56335026+kevinumarov@users.noreply.github.com>
2021-05-27 13:47:37 +0900

    Add files via upload

---
+++ 1.6/main.cpp
@@ -0,0 +1,20 @@
+// Created by Kevin, March 22,2021
+// Student number: 201739103
+// Introduction to Programming with C++
+/// Exercise 1.6
+/// Chapter 1.
+
+/* (Summation of odd numbers) Write a program that displays the sum of the first ten odd numbers
+ */
+
+// Pseudo code
+// I. Including the header files
+// II. Printing the result by using the cout statement
+// III. Display the output
+#include <iostream>
+using namespace std;
+int main() {
+    cout <<"Summation of odd numbers"<<endl;
+    cout <<"Sum of fist ten odd numbers is:" << 3 + 5 + 7 + 9 + 11 + 13 + 15 + 17 + 19 + 21 <<endl;
+    return 0;
+}
