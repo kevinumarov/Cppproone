fecbd43d796f248477da9228def4bbcd766c4913
Kevin <56335026+kevinumarov@users.noreply.github.com>
2021-05-27 14:01:41 +0900

    Add files via upload

---
+++ 2.5/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout <<"INT_MIN is : "<<INT_MIN<< endl; // there are constants, the value of INT_MIN never change because they never change
+    cout <<"INT_MAX is : "<<INT_MAX<<endl;
+    cout <<"LONG_MIN is : "<<LONG_MIN<< endl;
+    cout <<"LONG_MAX is : "<<LONG_MAX<<endl;
+    cout <<"FLT_MIN is : "<<__FLT_MIN__<< endl;
+    cout <<"FLT_MAX is : "<<__FLT_MAX__<<endl;
+    cout <<"DBL_MIN is : "<<__DBL_MIN__<< endl;
+    cout <<"DBl_MAX is : "<<__DBL_MAX__<<endl;
+    return 0;
+}
