1a44b3bc6ac3432c1be6e6a0304920af9cfcbeae
Kevin <56335026+kevinumarov@users.noreply.github.com>
2021-05-27 13:35:50 +0900

    Add files via upload

---
+++ 1.4/main.cpp
@@ -0,0 +1,31 @@
+// Created by Kevin, March 22,2021
+// Student number: 201739103
+// Introduction to Programming with C++
+/// Exercise 1.4,
+/// Chapter 1.
+
+/* (Print a table) Write a program that displays the following table:
+        p       p*5      p*10
+        5       25       50
+        10      50       100
+        25      125      250
+        50      250      500
+ */
+
+// Pseudo code
+// I. Including the header files
+// II. Printing the table by using the cout statement
+// III. Display the output
+
+#include <iostream>
+using namespace std;
+int main (){
+cout <<"p\tp*5\tp*10"<<endl;
+cout <<"5\t25\t50"<<endl;
+cout <<"10\t50\t100"<<endl;
+cout <<"25\t125\t250"<<endl;
+cout <<"50\t250\t500"<<endl;
+
+return 0;
+}
+
